9682a6fdb8a6e7d1f016b4d785ddaef9bbd1b57e
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-03-24 12:44:43 -0300

    Criando e requisitando dados da api fake com json serve

--- src/components/vendas/Leads.vue
+++ src/components/vendas/Leads.vue
@@ -1,3 +1,25 @@
 <template>
    <h3>Leads</h3>
+   {{dados}}
 </template>
+
+<script>
+export default {
+   name: 'Leads',
+   data: ()=>({
+      dados: null
+   }),
+
+   methods: {
+      getDadosApi(){
+         fetch('http://localhost:3000/leads')
+            .then(response => response.json())
+            .then(data => this.dados = data)
+      }
+   },
+
+   created(){
+      this.getDadosApi()
+   }
+}
+</script>
