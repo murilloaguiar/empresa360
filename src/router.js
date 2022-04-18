95cc1edaf04faba3ac4112bc2e2ba2bd384d51f1
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-04-18 12:23:12 -0300

    Criando apelido para rotas, redirecionando rotas e aplicando rota curinga

--- src/router.js
+++ src/router.js
@@ -10,6 +10,7 @@ import Lead from '@/components/vendas/Lead.vue'
 import Leads from '@/components/vendas/Leads.vue'
 import Login from '@/views/Login.vue'
 import Opcoes from '@/components/servicos/Opcoes.vue'
+import PaginaNaoEncontrada from '@/views/PaginaNaoEncontrada.vue'
 import Servico from '@/components/servicos/Servico.vue'
 import Servicos from '@/components/servicos/Servicos.vue'
 import Site from '@/views/Site.vue'
@@ -26,20 +27,21 @@ const routes = [
 
    {
       path: '/home', //caminho para o caminho do componente. meusite.com/home
+      alias: '/app',
       component: Home,
       children: [
          { path: 'vendas', component: Vendas, children: 
             [
                { path: 'leads', component: Leads, name: 'leads'}, //meusite.com/home/vendas/leads
-               { path: 'leads/:id', component: Lead, name: 'lead'}, //meusite.com/home/vendas/leads/id
+               { path: 'leads/:id', component: Lead, name: 'lead', alias: ['/l/:id','/pessoa/:id','/:id']}, //meusite.com/home/vendas/leads/id
                { path: 'contratos', component: Contratos, name: 'contratos'}, //meusite.com/home/vendas/contratos
-               { path: '', component: VendasPadrao}, //meusite.com/home/vendas/
+               { path: '', component: VendasPadrao, name: 'vendas'}, //meusite.com/home/vendas/
             ] 
          },
 
          { path: 'servicos', component: Servicos, name: 'servicos', children: 
             [
-               {path:':id', name: 'servico', components: 
+               {path:':id', name: 'servico', alias: '/s/:id', components: 
                   {
                      default: Servico,
                      opcoes: Opcoes,
@@ -48,6 +50,7 @@ const routes = [
                }
             ]
          },//meusite.com/home/servicos
+
          { path: 'dashboard', components: {
             default: Dashboard,
             rodape: DashboardRodape
@@ -59,6 +62,20 @@ const routes = [
       path: '/login', //meusite.com/login
       component: Login
    },
+   {path:'/redirecionamento-1', redirect: '/home/servicos'},
+   {path:'/redirecionamento-2', redirect: {name: 'leads'} },
+   {path:'/redirecionamento-3', redirect: '/home/vendas' },
+   {path:'/redirecionamento-4', redirect: {name: 'vendas'} },
+   {path:'/redirecionamento-5', redirect: to =>{
+         //podemos programar algo antes do redirecionamento ser efetivado
+         console.log(to)
+         //return '/home/vendas'
+         return {name: 'vendas'}
+      }
+   },
+
+   // {path: '/:catchAll(.*)*', redirect: '/'}
+   {path: '/:catchAll(.*)*', component: PaginaNaoEncontrada}
    
 ]
 
---
+++ src/views/PaginaNaoEncontrada.vue
@@ -0,0 +1,3 @@
+<template>
+   <h2>Ops...Essa página não foi encontrada</h2>
+</template>
