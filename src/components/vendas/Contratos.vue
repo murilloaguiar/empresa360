eb9f997700b9eb51e38d711080dd3f0cefcd6b26
Murillo Aguiar <aguiar_murillo@outlook.com>
2022-04-18 12:41:22 -0300

    Consulta com relacionamentos com o Json-server

--- src/components/vendas/Contratos.vue
+++ src/components/vendas/Contratos.vue
@@ -1,3 +1,43 @@
 <template>
-   <h3>Contratos</h3>
+   <div>
+      <h3>Contratos</h3>
+
+      <table class="table table-hover">
+
+         <thead>
+            <tr>
+               <th scope="col">ID</th>
+               <th scope="col">Lead</th>
+               <th scope="col">Serviço</th>
+               <th scope="col">Data Início</th>
+               <th scope="col">Data Fim</th>
+            </tr>
+         </thead>
+
+         <tbody>
+            <tr v-for="dado in dados" :key="dado.id">
+               <td>{{dado.id}}</td>
+               <td>{{dado.lead.nome}}</td>
+               <td>{{dado.servico.servico}}</td>
+               <td>{{dado.data_inicio}}</td>
+               <td>{{dado.data_fim}}</td>
+            </tr>
+         </tbody>
+
+      </table>
+
+   </div>
 </template>
+
+<script>
+import ApiMixin from '@/mixins/ApiMixin'
+
+export default {
+   name: 'Leads',
+   mixins: [ApiMixin],
+   
+   created(){
+      this.getDadosApi('http://localhost:3000/contratos?_expand=lead&_expand=servico')
+   }
+}
+</script>
